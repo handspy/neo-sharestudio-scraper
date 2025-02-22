dcfde01f7f6cef719623ffb823565f299c3d699e
José Carlos Paiva <josepaiva94@gmail.com>
2025-02-22 13:18:08 +0000

    delete trash

--- dist/constants.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const NEO_METADATA_FETCH_URL = "https://neonotes2-d0880.firebaseio.com";
-export declare const NEOINK_DATA_FETCH_URL = "https://firebasestorage.googleapis.com/v0/b/neonotes2-d0880.appspot.com/o";
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { HandSpyPage, NeoInkItem, NeoInkItemData, NeoNoteSize } from './types';
-export declare const fetchNeoInkItems: (id: string) => Promise<NeoInkItem[]>;
-export declare const fetchNeoInkItemData: (item: NeoInkItem) => Promise<NeoInkItemData | null>;
-export declare const fetchNeoNoteSize: (itemData: Partial<NeoInkItemData>) => Promise<NeoNoteSize>;
-export declare const fetchNeoPages: (id: string) => Promise<HandSpyPage[]>;
